36df9763e0eab064abc4590e3723a8085ea07707
Dela Naeini <delaram.naeini@gmail.com>
2023-07-22 12:16:29 -0700

    Removed unnecessary imports

--- src/TEST_EXERCISE_DATA.js
+++ src/TEST_EXERCISE_DATA.js
@@ -1,29 +1,29 @@
 export const TEST_EXERCISE_DATA = [
     {
-    id: 0,
-    name: "Lat Pulldown",
-    sets: 3,
-    reps: 15,
-    exerciseday: "M"
+        id: 0,
+        name: "Lat Pulldown",
+        sets: 3,
+        reps: 15,
+        exerciseday: "M"
     },
     {
-    id: 1,
-    name: "Cable Row",
-    sets: 3,
-    reps: 15,
-    exerciseday: "M"
+        id: 1,
+        name: "Cable Row",
+        sets: 3,
+        reps: 15,
+        exerciseday: "M"
     },
     {
-    id: 2,
-    name: "Leg Press",
-    sets: 3,
-    reps: 15,
-    exerciseday: "W"
+        id: 2,
+        name: "Leg Press",
+        sets: 3,
+        reps: 15,
+        exerciseday: "W"
     },
     {
-    id: 3,
-    name: "Leg Curl",
-    sets: 3,
-    reps: 12,
-    exerciseday: "W"
+        id: 3,
+        name: "Leg Curl",
+        sets: 3,
+        reps: 12,
+        exerciseday: "W"
     }];
--- src/features/WeekView.js
+++ src/features/WeekView.js
@@ -1,46 +1,52 @@
-import { Table, Card, CardHeader, CardBody, Button } from 'reactstrap';
-import { TEST_EXERCISE_DATA } from '../TEST_EXERCISE_DATA';
-import { useState } from 'react';
+import { Table, Card, CardHeader, CardBody, Button } from "reactstrap";
 
 const WeekView = ({ deleteExercise, exercise }) => {
-    const week = [
-        "M", "T", "W", "Th", "F", "Sa", "Su"];
+  const week = ["M", "T", "W", "Th", "F", "Sa", "Su"];
 
-
-    return (
-        <Card>
-            <CardHeader className='bg-success'>
-                <h3>Weekly Calendar</h3>
-            </CardHeader>
-            <CardBody>
-                <Table>
-                    <tr>{week.map((dayofweek) => <th>{dayofweek}</th>)}
-                    </tr>
-                    <tr>
-                        {week.map((dayofweek) => {
-                            return (
-                                <td>
-                                    {exercise.filter((exercise) => {
-                                        return (
-                                            exercise.exerciseday === dayofweek
-                                        );
-                                    }).map((exercise) => {
-                                        return (
-                                            <div>
-                                                {exercise.name}
-                                                {exercise.sets && exercise.reps && `: ${exercise.sets} x ${exercise.reps} `}
-                                                <Button className='btn-sm mb-2' onClick={() => deleteExercise(exercise.id)}>x</Button>
-                                            </div>
-                                        );
-                                    })}
-                                </td>
-                            )
-                        })}
-                    </tr>
-                </Table>
-            </CardBody>
-        </Card>
-    );
+  return (
+    <Card>
+      <CardHeader className="bg-success">
+        <h3>Weekly Calendar</h3>
+      </CardHeader>
+      <CardBody>
+        <Table>
+          <tr>
+            {week.map((dayofweek) => (
+              <th>{dayofweek}</th>
+            ))}
+          </tr>
+          <tr>
+            {week.map((dayofweek) => {
+              return (
+                <td>
+                  {exercise
+                    .filter((exercise) => {
+                      return exercise.exerciseday === dayofweek;
+                    })
+                    .map((exercise) => {
+                      return (
+                        <div>
+                          {exercise.name}
+                          {exercise.sets &&
+                            exercise.reps &&
+                            `: ${exercise.sets} x ${exercise.reps} `}
+                          <Button
+                            className="btn-sm mb-2"
+                            onClick={() => deleteExercise(exercise.id)}
+                          >
+                            x
+                          </Button>
+                        </div>
+                      );
+                    })}
+                </td>
+              );
+            })}
+          </tr>
+        </Table>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default WeekView;
